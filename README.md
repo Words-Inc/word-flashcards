f5e2cf94614fd93a9d5d1c5f8c676157bc5e8ff2
Cera Barrow <cera.barrow@gmail.com>
2022-11-14 15:34:24 -0800

    Initial boilerplate
    Co-authored-by: Tracy Chang <tracycchang@users.noreply.github.com>
    Co-authored-by: Rilo <WaffleCloud@users.noreply.github.com>
    Co-authored-by: Alex McPhail <mcphail.alex@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+#Word a Day
+
+
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const environment = process.env.NODE_ENV;
+
+module.exports = {
+	mode: environment,
+	entry: {
+		src: './client/index.js'
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build'),
+	},
+	module: {
+		rules: [
+			{
+		test: /\.jsx?/,
+		exclude: /node_modules/,
+		use: {
+		loader: 'babel-loader',
+		options: {
+		    presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			},
+			{
+        test: /\.(png|jpe?g|gif)$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+			{
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      }
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Development',
+			template: './client/index.html'
+		}),
+	],
+	devServer: {
+		static: {
+			publicPath: '/build',
+			directory: path.resolve(__dirname, 'build')
+		},
+		port: 8080,
+		proxy: {
+			'/api': 'http://localhost:3000/',
+		}
+	}
+}
